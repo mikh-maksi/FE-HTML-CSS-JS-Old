d210da8fc227aa4e4dc591effeb5e6bbb205e28f
newlevelcenter <newlevelcenter@gmail.com>
2016-09-27 21:43:32 +0300

    Ball

--- lesson_js_05-06/base/js/js_1.js
+++ lesson_js_05-06/base/js/js_1.js
@@ -2,38 +2,55 @@ start.addEventListener("click",start_click);
 pause.addEventListener("click",pause_click);
 stp.addEventListener("click",stp_click);
 
+
+
 var obj={
   s: "start",
   p: "pause",
   st: "stop"
 }
+
 var count=0, timerId, go=false;
+
 function start_click(){
-  for_text.innerHTML="start_click";
-  if (go==false){
-  timerId = setInterval(function() {
-  count++;
-  var msek=count%100;
-  var sek=Math.floor(count/100)%60;
-  if (sek<10) {sek = "0"+sek;}
-  var min = Math.floor(count/6000)%60;
-  var hours = Math.floor(count/360000)%24;;
-  var str = hours+":"+min+":"+sek+":"+msek;
-  for_count.innerHTML=str;
-  }, 1);
-  go=true;
+	console.log("start");
+	for_text.innerHTML="start_click";
+	  if (go==false){
+		  timerId = setInterval(function() {
+			  count++;
+			  var msek=count%100;
+			  
+			  if (msek<10) {msek = "0"+msek;}
+			  if ((msek>=10)&&(msek<100)) {msek = ""+msek;}
+			  
+			  var sek=Math.floor(count/100)%60;
+			  if (sek<10) {sek = "0"+sek;}
+			  
+			  var min = Math.floor(count/6000)%60;
+			  if (min<10) {min = "0"+min;}
+			  
+			  var hours = Math.floor(count/360000)%24;
+			  if (hours<10) {hours = "0"+hours;}
+			  
+			  var str = hours+":"+min+":"+sek+":"+msek;
+			  for_count.innerHTML=str;
+	  }, 1);
+	  go=true;
+	  }
 }
-}
+
 function pause_click(){
-  clearInterval(timerId);
-  go=false;
-  for_text.innerHTML="pause_click";
+	clearInterval(timerId);
+	go=false;
+	for_text.innerHTML="pause_click";
+	console.log("pause");
 }
 
 function stp_click(){
-  clearInterval(timerId);
-  go=false;
-  count = 0;
-  for_count.innerHTML=count;
-  for_text.innerHTML="stp_click";
+	console.log("stop");
+	clearInterval(timerId);
+	go=false;
+	count = 0;
+	for_count.innerHTML=count;
+	for_text.innerHTML="stp_click";
 }
