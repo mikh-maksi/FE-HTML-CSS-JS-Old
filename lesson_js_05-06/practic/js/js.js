16b08b04734f825f36c0e860cf0b9d15039b6c68
mikh-maksi <mihanik@ukr.net>
2016-12-06 09:46:06 +0200

    practic

---
+++ lesson_js_05-06/practic/js/js.js
@@ -0,0 +1,12 @@
+console.log("test");
+addEventListener("click",calculate);
+function calculate(){
+	console.log("calculate");
+	//объявление переменных и задание тестовых значений
+	var send, recive, service_type, send_type, wt, size_w, size_h, size_d,number,cost,pack,back;
+	 send=1, recive=1, service_type=1, send_type=1, wt=1, size_w=1, size_h=1, size_d=1,number=1,cost=1,pack=1,back=1;
+	//получение значений, вывод их на экран.
+	
+	//задание формулы.
+}
+
